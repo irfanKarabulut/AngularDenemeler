e5309b623d1f6e402f014173e274e2c5a1f1dc5a
irfankarabulut86 <irfankarabulut86@gmail.com>
2024-11-19 00:28:25 +0300

    html oluşturuldu, todo ekleme ve silme  işlemleri yapıldı.

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-app';
+
+  todoItem: string = "";
+  todoItems: string[] = [];
+
+  save() {
+    this.todoItems.push(this.todoItem);
+    this.todoItem = "";
+  }
+
+  delete(index: number) {
+    this.todoItems.splice(index, 1);
+  }
 }
