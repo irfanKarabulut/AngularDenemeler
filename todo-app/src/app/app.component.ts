5cb2bdb47a3d2c338b2cd7970485e60369d9abe5
irfanKarabulut <irfankarabulut86@gmail.com>
2024-11-24 00:03:12 +0300

    getbootstrap eklendi. localstorage yapısı kullanıldı.

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,40 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FormsModule],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-app';
-
-  todoItem: string = "";
-  todoItems: string[] = [];
-  updateMode: boolean = false;
-  updateIndex: number = -1;
-
-  save() {
-    if (!this.updateMode) {
-      this.todoItems.push(this.todoItem);
-    } else {
-      this.todoItems[this.updateIndex] = this.todoItem;
-      this.updateIndex = -1;
-      this.updateMode = false;
-    }
-    this.todoItem = "";
-  }
-
-  delete(index: number) {
-    this.todoItems.splice(index, 1);
-  }
-
-  select(index: number) {
-    this.todoItem = this.todoItems[index];
-    this.updateMode = true;
-    this.updateIndex = index;
-  }
 }
--- todo-app/src/app/home/home.component.ts
+++ todo-app/src/app/home/home.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  todoItem: string = "";
+  todoItems: string[] = [];
+  updateMode: boolean = false;
+  updateIndex: number = -1;
+ 
+  constructor() {
+    let todos = localStorage.getItem("todos");
+    if(todos)
+      this.todoItems = JSON.parse(todos);
+  }
+
+  save() {
+    if (!this.updateMode) {
+      this.todoItems.push(this.todoItem);
+    } else {
+      this.todoItems[this.updateIndex] = this.todoItem;
+      this.updateIndex = -1;
+      this.updateMode = false;
+    }
+    this.todoItem = "";
+    this.setLocalStorage();
+  }
+
+  delete(index: number) {
+    this.todoItems.splice(index, 1);
+    this.setLocalStorage();
+  }
+
+  select(index: number) {
+    this.todoItem = this.todoItems[index];
+    this.updateMode = true;
+    this.updateIndex = index;
+  }
+
+  setLocalStorage(){
+    localStorage.setItem("todos", JSON.stringify(this.todoItems))
+  }
 
 }
